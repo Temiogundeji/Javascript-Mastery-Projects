8427b7a1926d850c1b06fbec4ab9d4e124d82c84
Temi Ogundeji <yusuffogundeji@gmail.com>
2025-03-02 14:55:14 +0100

    complete convertToNumeral

--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -2,6 +2,21 @@
 let romanNumeralEquivalent = "";
 //Number to convert
 let numToConvert = 3220;
+const romanNumerals = {
+  1: "I",
+  4: "IV",
+  5: "V",
+  9: "IX",
+  10: "X",
+  40: "XL",
+  50: "L",
+  90: "XC",
+  100: "C",
+  400: "CD",
+  500: "D",
+  900: "CM",
+  1000: "M",
+};
 
 function breakToPlaceValue(numToConvert = 3412) {
   let splittedNumToConvert = numToConvert
@@ -26,12 +41,26 @@ function breakToPlaceValue(numToConvert = 3412) {
 }
 
 function convertToRomanNumeral(number) {
+  if (number >= 4000) {
+    throw new Error("Number must not be upto 4000");
+  } else if (number < 0 || typeof number === "String") {
+    throw new Error("Number cannot be less than zero or string");
+  }
   let brokenNumber = breakToPlaceValue(number);
-  romanNumeralEquivalent = brokenNumber.map((value, index) => {});
-  console.log(brokenNumber);
+  brokenNumber.map((value, index) => {
+    if (romanNumerals[value]) {
+      romanNumeralEquivalent += romanNumerals[value];
+    } else {
+      let power = Math.pow(10, 3 - index);
+      let romanNumeralChars = romanNumerals[power].repeat(value / power);
+      romanNumeralEquivalent += romanNumeralChars;
+    }
+  });
+  console.log(romanNumeralEquivalent);
+  return romanNumeralEquivalent;
 }
 
-convertToRomanNumeral(2345);
+convertToRomanNumeral(3125);
 
 // Determine the hundredth, Tenth and Units
 // Convert each value into its equivalents considering its positions
