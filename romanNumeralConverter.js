4d1d855a63fbd3d9c0b6ee000ac135ffe934a3d5
Temi Ogundeji <yusuffogundeji@gmail.com>
2025-03-02 15:12:57 +0100

    remove comments

--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -61,20 +61,3 @@ function convertToRomanNumeral(number) {
 }
 
 convertToRomanNumeral(3125);
-
-// Determine the hundredth, Tenth and Units
-// Convert each value into its equivalents considering its positions
-// Make case for repition rule:
-// When a value is a single digit whole number, merge its equivalent to romanNumeralEquivalent string variable.
-// When a value is a double digit whole number, break it down into its constituents and merge it to romanNumeralEquivalent string variable
-// When a value is tripple digit whole number, break it down into its constituents and merge it to romanNumeralEquivalent string variable
-// When a value is divided by hundred, ten, or unit. And the remainder is zero, repeat the roman numeral equivalent Thousands, Hundred, Tenths and unit the number of time equivalent to the value divided value: hundred, tenth, units.
-// Make case for subraction rule
-// If number lesser by hundreds, tenths, and units doesn't return any of (VXLCDM), the value returned is gotten by moving the roman numeral equivalent of the divisor to the back.
-// Make case for addtion rule
-// For numbers not following the above convention break it down into its constituent using hundreds, tens and unit and merge them.
-//Other rules:
-//- I can only subtract from V (5) and X (10).
-//- X can only subtract from L (50) and C (100).
-//- C can only subtract from D (500) and M (1000)
-//- The same symbol can be repeated up to three times, but not more (e.g., IIII is incorrect, use IV for 4).
